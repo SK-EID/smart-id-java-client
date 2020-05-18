550617bf5afdaa72aa43d6b2a5017592528805d3
Juhan Aasaru <Juhan.Aasaru@nortal.com>
2020-05-18 12:10:50 +0300

    move SSL verification tests to separate class, improve tests

--- src/test/java/ee/sk/smartid/SmartIdIntegrationTest.java
+++ src/test/java/ee/sk/smartid/SmartIdIntegrationTest.java
@@ -26,18 +26,10 @@ package ee.sk.smartid;
  * #L%
  */
 
-import ee.sk.smartid.exception.CertificateNotFoundException;
 import org.apache.commons.codec.binary.Base64;
 import org.junit.Before;
 import org.junit.Test;
 
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManagerFactory;
-import javax.ws.rs.ProcessingException;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import java.io.InputStream;
-import java.security.KeyStore;
 import java.security.cert.CertificateEncodingException;
 
 import static org.hamcrest.Matchers.isEmptyOrNullString;
@@ -55,9 +47,6 @@ public class SmartIdIntegrationTest {
     private static final String CERTIFICATE_LEVEL_QUALIFIED = "QUALIFIED";
     private SmartIdClient client;
 
-    private static final String
-            DEMO_HOST_SSL_CERTIFICATE =
-            "-----BEGIN CERTIFICATE-----\nMIIFIjCCBAqgAwIBAgIQBH3ZvDVJl5qtCPwQJSruujANBgkqhkiG9w0BAQsFADBNMQswCQYDVQQG\nEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMScwJQYDVQQDEx5EaWdpQ2VydCBTSEEyIFNlY3Vy\nZSBTZXJ2ZXIgQ0EwHhcNMTcwODAxMDAwMDAwWhcNMjAxMDAyMTIwMDAwWjB0MQswCQYDVQQGEwJF\nRTEQMA4GA1UEBxMHVGFsbGlubjEbMBkGA1UEChMSU0sgSUQgU29sdXRpb25zIEFTMR0wGwYDVQQL\nExRWYWx1ZS1hZGRlZCBTZXJ2aWNlczEXMBUGA1UEAxMOc2lkLmRlbW8uc2suZWUwggEiMA0GCSqG\nSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDGFOOk4KzH95NP2dWUuPIvv4RGj3Zvk/Y3ZwavDaPzUkKS\nY9jgiI8EHYIiKde10bqfMeZ1N4No2orwzTtzMP2rqLwGd8ZYSFyF8pymxx0TY0w4yP1MWOq+MQ/6\n5fdBOgOXyhEoIHVWbbAJMmJaH0ozyKwXSKElHNzvKemDTHt7i/NKRG6oBexl3y6KEzKU37mg+scV\nr1i9hPlSO+ymvVUN+VCr1GteNuiFcpRdToVl9rXjvD2mqZfokD5VOuwPwuOecIIqjTpd87kzlgka\nlQfijx1jOBwVx2Hx+wgASiMy2cfHqXlkBvpvi4HTvjK/DMv4C2AqKJHlwjShceuESCH7AgMBAAGj\nggHVMIIB0TAfBgNVHSMEGDAWgBQPgGEcgjFh1S8o541GOLQs4cbZ4jAdBgNVHQ4EFgQUvTSpJnBN\ntfuuL2YY3AKaIPxXljMwGQYDVR0RBBIwEIIOc2lkLmRlbW8uc2suZWUwDgYDVR0PAQH/BAQDAgWg\nMB0GA1UdJQQWMBQGCCsGAQUFBwMBBggrBgEFBQcDAjBrBgNVHR8EZDBiMC+gLaArhilodHRwOi8v\nY3JsMy5kaWdpY2VydC5jb20vc3NjYS1zaGEyLWcxLmNybDAvoC2gK4YpaHR0cDovL2NybDQuZGln\naWNlcnQuY29tL3NzY2Etc2hhMi1nMS5jcmwwTAYDVR0gBEUwQzA3BglghkgBhv1sAQEwKjAoBggr\nBgEFBQcCARYcaHR0cHM6Ly93d3cuZGlnaWNlcnQuY29tL0NQUzAIBgZngQwBAgIwfAYIKwYBBQUH\nAQEEcDBuMCQGCCsGAQUFBzABhhhodHRwOi8vb2NzcC5kaWdpY2VydC5jb20wRgYIKwYBBQUHMAKG\nOmh0dHA6Ly9jYWNlcnRzLmRpZ2ljZXJ0LmNvbS9EaWdpQ2VydFNIQTJTZWN1cmVTZXJ2ZXJDQS5j\ncnQwDAYDVR0TAQH/BAIwADANBgkqhkiG9w0BAQsFAAOCAQEAtr3K/2vKMH75bbEKrEorjxEsEOQo\npcIhBU5mOVVU5XO+xlrL6NvjyCV47Z9N+uEq4X59YTki23/NGMS85Mm+gl1wq8oPRdNSpNAVhrNY\nNYSFYkvVdFELKmVkep53D2YiB0ygOWghk9JI6UX/kWxBr5N2Qc4+eRKAjlm3vf/HGmOaG2LRbSLL\nPmp6VDQebv2P53rqYdzUpR/qQWHyMtTnku/i0eCY1UCkZHoxLV5vbztAT9kWS0s1d38yDqfljGSW\n/jbdu3P2jkR6PhH5Lupe24SN7jpKDfQJ8oDx6RTM8op7BvL67e6bVW8PzYZCI5BW7ZxEq85+2zIL\nwcEt/pk+DA==\n-----END CERTIFICATE-----";
 
     @Before
     public void setUp() {
@@ -145,160 +134,6 @@ public class SmartIdIntegrationTest {
         AuthenticationResponseValidator authenticationResponseValidator = new AuthenticationResponseValidator();
         SmartIdAuthenticationResult authenticationResult = authenticationResponseValidator.validate(authenticationResponse);
         assertAuthenticationResultValid(authenticationResult);
-    }
-
-    @Test
-    public void makeRequestToApi_useDefaultSSLContext_sslHandshakeSucceedsFetchesCertificate() {
-        client
-             .getCertificate()
-             .withRelyingPartyUUID(RELYING_PARTY_UUID)
-             .withRelyingPartyName(RELYING_PARTY_NAME)
-             .withDocumentNumber(DOCUMENT_NUMBER)
-             .withCertificateLevel(CERTIFICATE_LEVEL_QUALIFIED)
-             .fetch();
-    }
-
-    @Test(expected = ProcessingException.class)
-    public void makeRequestToWrongApi_useDefaultSSLContext_sslHandshakeFailsThrowsException() {
-        client.setHostUrl("https://tsp.demo.sk.ee/mid-api");
-        client
-             .getCertificate()
-             .withRelyingPartyUUID(RELYING_PARTY_UUID)
-             .withRelyingPartyName(RELYING_PARTY_NAME)
-             .withDocumentNumber(DOCUMENT_NUMBER)
-             .withCertificateLevel(CERTIFICATE_LEVEL_QUALIFIED)
-             .fetch();
-    }
-
-    @Test(expected = ProcessingException.class)
-    public void makeRequestToWrongApi_useCustomSSLContext_sslHandshakeFailsThrowsException() {
-        client.setHostUrl("https://tsp.demo.sk.ee/mid-api");
-        client.addTrustedSSLCertificates(DEMO_HOST_SSL_CERTIFICATE);
-        client
-             .getCertificate()
-             .withRelyingPartyUUID(RELYING_PARTY_UUID)
-             .withRelyingPartyName(RELYING_PARTY_NAME)
-             .withDocumentNumber(DOCUMENT_NUMBER)
-             .withCertificateLevel(CERTIFICATE_LEVEL_QUALIFIED)
-             .fetch();
-    }
-
-    @Test(expected = ProcessingException.class)
-    public void makeRequestToWrongApi_createConfiguredJaxwsClientWithDemoSSLContext_sslHandshakeFailsThrowsException() throws Exception {
-        InputStream is = SmartIdIntegrationTest.class.getResourceAsStream("/demo_ssl_cert.jks");
-        KeyStore keyStore = KeyStore.getInstance("JKS");
-        keyStore.load(is, "changeit".toCharArray());
-
-        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance("X509");
-        trustManagerFactory.init(keyStore);
-
-        SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
-        sslContext.init(null, trustManagerFactory.getTrustManagers(), null);
-
-        Client configuredClient = ClientBuilder.newBuilder().sslContext(sslContext).build();
-        client.setConfiguredClient(configuredClient);
-        client.setHostUrl("https://tsp.demo.sk.ee/mid-api");
-        client
-             .getCertificate()
-             .withRelyingPartyUUID(RELYING_PARTY_UUID)
-             .withRelyingPartyName(RELYING_PARTY_NAME)
-             .withDocumentNumber(DOCUMENT_NUMBER)
-             .withCertificateLevel(CERTIFICATE_LEVEL_QUALIFIED)
-             .fetch();
-    }
-
-    @Test
-    public void makeRequestApi_createConfiguredJaxwsClientWithDemoSSLContext_sslHandshakeSucceedsButExeptionIsThrown() throws Exception {
-        InputStream is = SmartIdIntegrationTest.class.getResourceAsStream("/demo_ssl_cert.jks");
-        KeyStore keyStore = KeyStore.getInstance("JKS");
-        keyStore.load(is, "changeit".toCharArray());
-
-        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance("X509");
-        trustManagerFactory.init(keyStore);
-
-        SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
-        sslContext.init(null, trustManagerFactory.getTrustManagers(), null);
-
-        Client configuredClient = ClientBuilder.newBuilder().sslContext(sslContext).build();
-
-        client.setConfiguredClient(configuredClient);
-        client
-             .getCertificate()
-             .withRelyingPartyUUID(RELYING_PARTY_UUID)
-             .withRelyingPartyName(RELYING_PARTY_NAME)
-             .withDocumentNumber(DOCUMENT_NUMBER)
-             .withCertificateLevel(CERTIFICATE_LEVEL_QUALIFIED)
-             .fetch();
-    }
-
-    @Test(expected = CertificateNotFoundException.class)
-    public void makeRequestToDemoEnv_useDefaultSslContext_sslHandshakeSucceedsButThrowsCertificateNotFound() {
-        client.setHostUrl("https://sid.demo.sk.ee/smart-id-rp/v1/");
-        client
-             .getCertificate()
-             .withRelyingPartyUUID(RELYING_PARTY_UUID)
-             .withRelyingPartyName(RELYING_PARTY_NAME)
-             .withDocumentNumber(DOCUMENT_NUMBER)
-             .fetch();
-    }
-
-    @Test
-    public void makeRequestToApi_loadCertificatesFromKeyStore_sslHandshakeSucceedsAndCertificateRetrieved() throws Exception {
-        InputStream is = SmartIdIntegrationTest.class.getResourceAsStream("/demo_ssl_cert.jks");
-        KeyStore keyStore = KeyStore.getInstance("JKS");
-        keyStore.load(is, "changeit".toCharArray());
-
-        client.loadSslCertificatesFromKeystore(keyStore);
-
-        client
-             .getCertificate()
-             .withRelyingPartyUUID(RELYING_PARTY_UUID)
-             .withRelyingPartyName(RELYING_PARTY_NAME)
-             .withDocumentNumber(DOCUMENT_NUMBER)
-             .fetch();
-    }
-
-    @Test(expected = ProcessingException.class)
-    public void makeRequestToApi_emptyKeyStore_sslHandshakeFailsAndThrowsException() throws Exception {
-        KeyStore keyStore = KeyStore.getInstance("JKS");
-        keyStore.load(null, null);
-
-        client.loadSslCertificatesFromKeystore(keyStore);
-
-        client
-             .getCertificate()
-             .withRelyingPartyUUID(RELYING_PARTY_UUID)
-             .withRelyingPartyName(RELYING_PARTY_NAME)
-             .withDocumentNumber(DOCUMENT_NUMBER)
-             .fetch();
-    }
-
-    @Test(expected = ProcessingException.class)
-    public void makeRequestToApi_loadCertificatesFromKeyStore_wrongCertificate_sslHandshakeFailsAndThrowsException() throws Exception {
-        InputStream is = SmartIdIntegrationTest.class.getResourceAsStream("/wrong_ssl_cert.jks");
-        KeyStore keyStore = KeyStore.getInstance("JKS");
-        keyStore.load(is, "changeit".toCharArray());
-
-        client.loadSslCertificatesFromKeystore(keyStore);
-
-        client
-             .getCertificate()
-             .withRelyingPartyUUID(RELYING_PARTY_UUID)
-             .withRelyingPartyName(RELYING_PARTY_NAME)
-             .withDocumentNumber(DOCUMENT_NUMBER)
-             .fetch();
-    }
-
-    @Test
-    public void makeRequestToApi_useDemoEnvCertificates_sslHandshakeSuccessFetchesCertificate() {
-        client.useDemoEnvSSLCertificates();
-
-        client
-             .getCertificate()
-             .withRelyingPartyUUID(RELYING_PARTY_UUID)
-             .withRelyingPartyName(RELYING_PARTY_NAME)
-             .withDocumentNumber(DOCUMENT_NUMBER)
-             .fetch();
     }
 
     private void assertSignatureCreated(SmartIdSignature signature) {
