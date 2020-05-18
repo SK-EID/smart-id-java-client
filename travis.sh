fe68172100da57f8b9ea5d1f683e1a2c2171f8cd
Juhan Aasaru <Juhan.Aasaru@nortal.com>
2020-05-18 10:12:54 +0300

    fix spaces in test

--- travis.sh
+++ travis.sh
@@ -7,7 +7,7 @@ echo "Is pull request: $TRAVIS_PULL_REQUEST"
 echo "Tag:             $TRAVIS_TAG"
 echo "JDK version:     $TRAVIS_JDK_VERSION"
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ] && ["$TRAVIS_JDK_VERSION" == "openjdk8"]; then
+if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ] && [ "$TRAVIS_JDK_VERSION" == "openjdk8" ]; then
   echo "Starting to publish"
   ./publish.sh
   echo "Finished"
