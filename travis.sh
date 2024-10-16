42d74f471d641f44a692b431be4d4a8bad0e24ba
Urmas Muser <urmas.muser@nortal.com>
2024-10-16 14:35:38 +0300

    SLIB-61 - fix NVD_key usage

--- travis.sh
+++ travis.sh
@@ -13,7 +13,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ] && [ "$TRAVI
   echo "Finished"
 elif [ "$TRAVIS_JDK_VERSION" == "openjdk17" ]; then
   ./mvnw test
-  ./mvnw -DnvdApiKey=$NVD_key org.owasp:dependency-check-maven:check
+  ./mvnw -DnvdApiKey="$NVD_key" org.owasp:dependency-check-maven:check
   ./mvnw spotbugs:check
 else
   ./mvnw test
